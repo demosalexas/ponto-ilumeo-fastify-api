4e7f58a7468e328e3c2981daf9bd778c7aeb93f3
Matheus R. F. Ferreira <mrff@tuta.io>
2024-05-21 07:35:51 -0300

    fix app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AttendancesModule } from './attendances/attendances.module';
 
 @Module({
   imports: [AttendancesModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
